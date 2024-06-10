7732ca3578b10b956474fdd4eb73856fc5c76a43
NASUNJIN <sj010618@gmail.com>
2024-06-11 03:14:23 +0900

    UPLOAD try-catch

---
+++ demo-js/try-catch-demo.js
@@ -0,0 +1,21 @@
+let string = '{"num1":1 }';
+
+try {
+    // username;
+    let json = JSON.parse(string);
+    
+    if(!json.name) {
+        throw new SyntaxError("입력값에 이름이 없음")
+    } else {
+        console.log(json.name)  // js 입장에선 에러 but, 우리 입장에선 에러 = 입력값이 잘못된 에러
+    }
+
+    let name = json.name;
+    console.log(name);
+
+} catch(err) {
+    console.log(err.name);
+    console.log(err.message);
+
+    console.log(err);
+}
